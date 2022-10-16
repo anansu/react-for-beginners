5394ba9939176691bf2e526a9d81f02fbda680cb
anansu <44854857+anansu@users.noreply.github.com>
2022-10-16 20:50:42 +0900

    Updates

---
+++ precache-manifest.feb5e2ea93a2f1bfa701be4f57d2c8a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "989aa89d6b3e21c2527b",
+    "url": "/react-for-beginners/static/js/main.989aa89d.chunk.js"
+  },
+  {
+    "revision": "23545b0c3c2324714882",
+    "url": "/react-for-beginners/static/js/1.23545b0c.chunk.js"
+  },
+  {
+    "revision": "93ab4fdb2861308b05c8",
+    "url": "/react-for-beginners/static/js/runtime~main.93ab4fdb.js"
+  },
+  {
+    "revision": "35255fbc822ae63c8fd3e0553bb8a3d1",
+    "url": "/react-for-beginners/index.html"
+  }
+];
